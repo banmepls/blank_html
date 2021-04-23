a31e7501b827214f921383e7e2a2d9ee4bfe343d
banmepls <68112969+banmepls@users.noreply.github.com>
2021-04-23 07:54:00 +0300

    Update js.js

--- scripts/js.js
+++ scripts/js.js
@@ -24,7 +24,7 @@ let loader = new THREE.GLTFLoader();
 let obj = null;
 
 loader.load('model/scene.gltf', function(gLtf) {
-	obj = gltf;
+	obj = gLtf;
 	obj.scene.scale.set(1.3, 1.3, 1.3);
 
 	scene.add(obj.scene);
